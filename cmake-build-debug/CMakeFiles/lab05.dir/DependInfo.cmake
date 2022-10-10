c30df9c94236935230c646d664b41b323237f06d
bschreiber8 <bschreiber@mines.edu>
2022-10-09 18:47:54 -0600

    Added ability to toggle between freecam and arcball cam

---
+++ cmake-build-debug/CMakeFiles/lab05.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/benschreiber/CLionProjects/ComputerGraphics/Graphics-MP/A3Engine.cpp" "/Users/benschreiber/CLionProjects/ComputerGraphics/Graphics-MP/cmake-build-debug/CMakeFiles/lab05.dir/A3Engine.cpp.o"
+  "/Users/benschreiber/CLionProjects/ComputerGraphics/Graphics-MP/ArcBallCam.cpp" "/Users/benschreiber/CLionProjects/ComputerGraphics/Graphics-MP/cmake-build-debug/CMakeFiles/lab05.dir/ArcBallCam.cpp.o"
+  "/Users/benschreiber/CLionProjects/ComputerGraphics/Graphics-MP/Raft.cpp" "/Users/benschreiber/CLionProjects/ComputerGraphics/Graphics-MP/cmake-build-debug/CMakeFiles/lab05.dir/Raft.cpp.o"
+  "/Users/benschreiber/CLionProjects/ComputerGraphics/Graphics-MP/main.cpp" "/Users/benschreiber/CLionProjects/ComputerGraphics/Graphics-MP/cmake-build-debug/CMakeFiles/lab05.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/lab05.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab05.dir/A3Engine.cpp.o"
+  "CMakeFiles/lab05.dir/ArcBallCam.cpp.o"
+  "CMakeFiles/lab05.dir/Raft.cpp.o"
+  "CMakeFiles/lab05.dir/main.cpp.o"
+  "lab05"
+  "lab05.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab05.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
